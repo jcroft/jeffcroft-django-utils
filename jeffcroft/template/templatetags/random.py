3cf0111ac8b2b151f615d94179453e0622d16631
Jeff Croft <jeff@jeffcroft.com>
2011-09-30 10:41:20 -0700

    Changing several_random filter to work only with QuerySets and act accordingly (saves tons of db queries).

--- jeffcroft/template/templatetags/random.py
+++ jeffcroft/template/templatetags/random.py
@@ -5,17 +5,10 @@ from django import template
 register = template.Library()
 
 @register.filter
-def several_random(value, arg=1):
-  "Returns one or more random item(s) from the list"
+def several_random(queryset, arg=1):
+  "Returns one or more random item(s) from a QuerySet"
   try:
     arg = int(arg)
   except ValueError:
-    return value
-  if arg == 1:    
-    return random_module.choice(value)
-  elif len(value) > arg:  # Only pick if we are asked for fewer items than we are given
-    return random_module.sample(value, arg)
-  else:   # Number requested is equal to or greater than the number we have, return them all in random order
-    new_list = list(value)
-    random_module.shuffle(new_list)
-    return new_list
+    return queryset
+  return queryset.order_by('?')[:arg]
