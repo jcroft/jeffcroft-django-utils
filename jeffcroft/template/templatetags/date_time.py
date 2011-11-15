9b0397d5db047135c68897b54362c28fe16a7d70
Jeff Croft <jeff@jeffcroft.com>
2011-11-14 22:57:06 -0800

    Fixing template tags to fail silently.

--- jeffcroft/template/templatetags/date_time.py
+++ jeffcroft/template/templatetags/date_time.py
@@ -71,58 +71,63 @@ def fuzzy_time(time):
 
 @register.filter  
 def fuzzy_timesince(start_time):
-  delta = datetime.datetime.now() - start_time
+  try:
+    delta = datetime.datetime.now() - start_time
 
-  plural = lambda x: 's' if x != 1 else ''
+    plural = lambda x: 's' if x != 1 else ''
 
-  num_years = delta.days / 365
-  if (num_years > 0):
-    return "%d year%s" % (num_years, plural(num_years))
+    num_years = delta.days / 365
+    if (num_years > 0):
+      return "%d year%s" % (num_years, plural(num_years))
 
-  num_weeks = delta.days / 7
-  if (num_weeks > 0):
-    return "%d week%s" % (num_weeks, plural(num_weeks))
+    num_weeks = delta.days / 7
+    if (num_weeks > 0):
+      return "%d week%s" % (num_weeks, plural(num_weeks))
 
-  if (delta.days > 0):
-    return "%d day%s" % (delta.days, plural(delta.days))
+    if (delta.days > 0):
+      return "%d day%s" % (delta.days, plural(delta.days))
 
-  num_hours = delta.seconds / 3600
-  if (num_hours > 0):
-    return "%d hour%s" % (num_hours, plural(num_hours))
+    num_hours = delta.seconds / 3600
+    if (num_hours > 0):
+      return "%d hour%s" % (num_hours, plural(num_hours))
 
-  num_minutes = delta.seconds / 60
-  if (num_minutes > 0):
-    return "%d minute%s" % (num_minutes, plural(num_minutes))
+    num_minutes = delta.seconds / 60
+    if (num_minutes > 0):
+      return "%d minute%s" % (num_minutes, plural(num_minutes))
 
-  return "a few seconds"
-
+    return "a few seconds"
+  except:
+    return '' #Fail silently
 
 
 
 @register.filter  
 def fuzzy_timeuntil(start_time):
-  if type(start_time) == type(datetime.date.today()):
-    start_time = datetime.datetime(start_time.year, start_time.month, start_time.day)
+  try:
+    if type(start_time) == type(datetime.date.today()):
+      start_time = datetime.datetime(start_time.year, start_time.month, start_time.day)
 
-  delta = start_time - datetime.datetime.now()
+    delta = start_time - datetime.datetime.now()
 
-  num_years = delta.days / 365
-  if (num_years > 0):
-    return "%d year%s" % (num_years, plural(num_years))
+    num_years = delta.days / 365
+    if (num_years > 0):
+      return "%d year%s" % (num_years, plural(num_years))
 
-  num_weeks = delta.days / 7
-  if (num_weeks > 0):
-    return "%d week%s" % (num_weeks, plural(num_weeks))
+    num_weeks = delta.days / 7
+    if (num_weeks > 0):
+      return "%d week%s" % (num_weeks, plural(num_weeks))
 
-  if (delta.days > 0):
-    return "%d day%s" % (delta.days, plural(delta.days))
+    if (delta.days > 0):
+      return "%d day%s" % (delta.days, plural(delta.days))
 
-  num_hours = delta.seconds / 3600
-  if (num_hours > 0):
-    return "%d hour%s" % (num_hours, plural(num_hours))
+    num_hours = delta.seconds / 3600
+    if (num_hours > 0):
+      return "%d hour%s" % (num_hours, plural(num_hours))
 
-  num_minutes = delta.seconds / 60
-  if (num_minutes > 0):
-    return "%d minute%s" % (num_minutes, plural(num_minutes))
+    num_minutes = delta.seconds / 60
+    if (num_minutes > 0):
+      return "%d minute%s" % (num_minutes, plural(num_minutes))
 
-  return "a few seconds"
+    return "a few seconds"
+  except:
+    return '' #Fail silently
