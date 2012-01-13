9552ef7260f382af54d99e081642c7ca834f3184
Jeff Croft <jeff@jeffcroft.com>
2012-01-13 15:36:40 -0800

    Adding a handy filter.

---
+++ jeffcroft/template/templatetags/parsing.py
@@ -0,0 +1,27 @@
+from django import template
+from django.utils.safestring import mark_safe
+ 
+register = template.Library()
+
+@register.filter
+def get_links(value):
+  """
+  Returns links found in an (X)HTML string as Python objects for iteration in templates.
+
+    <ul>
+      {% for link in object.body|markdown|get_links %}
+        <li><a href="{{ link.href }}">{{ link.title }}</a></li>
+      {% endfor %}
+    </ul>
+
+  """
+  
+  try:
+    from BeautifulSoup import BeautifulSoup
+    import urllib2
+  except ImportError:
+    if settings.DEBUG:
+      raise template.TemplateSyntaxError, "Error in {% get_links %} filter: The Python BeautifulSoup and/or urllib2 libraries aren't installed."
+    return value
+  soup = BeautifulSoup(value)
+  return soup.findAll('a')
