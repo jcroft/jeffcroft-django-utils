def5a16a0c356911ee52585e0451104b19c591b4
Jeff Croft <jeff@jeffcroft.com>
2012-01-25 18:26:45 -0800

    Debugging

--- jeffcroft/db/fields.py
+++ jeffcroft/db/fields.py
@@ -323,9 +323,8 @@ class PickledObjectField(models.Field):
         return 'TextField'
 
     def get_db_prep_lookup(self, lookup_type, value, connection=None, prepared=False):
-        if lookup_type not in ['exact', 'in', 'isnull']:
-            return None
-            # raise TypeError('Lookup type %s is not supported.' % lookup_type)
+        if lookup_type not in ['exact', 'in', 'isnull', 'contains', 'icontains']:
+            raise TypeError('Lookup type %s is not supported.' % lookup_type)
         # The Field model already calls get_db_prep_value before doing the
         # actual lookup, so all we need to do is limit the lookup types.
         try:
