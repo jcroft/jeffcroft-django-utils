ba57d05bcc953cf8ef026970437fb945e4c74c4c
Jeff Croft <jeff@jeffcroft.com>
2012-05-29 21:37:10 -0700

    Adding a decode_entities filter.

--- jeffcroft/template/templatetags/encoding.py
+++ jeffcroft/template/templatetags/encoding.py
@@ -18,3 +18,31 @@ def unescape(value):
   value = value.replace('&lt;br /&gt;', '<br />')
   value = value.replace('&amp;', '&')
   return value
+
+
+
+
+
+def _replace_entity(m): 
+  entity = m.group(3) 
+  if m.group(1) == '#': 
+      val = int(entity, m.group(2) == '' and 10 or 16) 
+  else: 
+      val = name2codepoint[entity] 
+  return unichr(val)
+
+@register.filter
+def deconde_entities(value):
+  """ 
+  Replaces HTML entities with unicode equivalents. 
+  Ampersands, quotes and carets are not replaced. 
+   
+  """ 
+  import re
+  from htmlentitydefs import name2codepoint 
+  from django.utils.encoding import force_unicode 
+
+  entity_re = re.compile("&(#?)([Xx]?)(\d+|[A-Fa-f0-9]+|%s);" % '|'.join(name2codepoint)) 
+  entity_no_escape_chars_re = re.compile(r"&(#?)([Xx]?)((?!39;)(\d+|[A-Fa-f0-9]+)|%s);" % '|'.join([k for k in name2codepoint if k not in ('amp', 'lt', 'gt', 'quot')])) 
+  regexp = decode_all and entity_re or entity_no_escape_chars_re 
+  return regexp.sub(_replace_entity, force_unicode(html)) 
